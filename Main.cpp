f1ec289c48b8545701f4948153d8220549b3c79b
ExplosiveGreen <69218745+ExplosiveGreen@users.noreply.github.com>
2021-04-25 04:28:21 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <stdlib.h>
+#include <time.h> 
 using namespace std;
 
 #include "NumberWithUnits.hpp"
@@ -21,30 +23,22 @@ void get_file(string& str){
         cout << "Input failed - no more input?" << endl;
     }
 }
-
-int main() {
-    string file;
-    get_file(file);
-    
-    ifstream units{file};
-    while(units.fail()){
-        cout << "reading from " << file << " failed." << endl;
-        get_file(file);
-        units.open(file);
-    }
-    NumberWithUnits::read_units(units);
-    NumberWithUnits a;
-    NumberWithUnits b;
+void randomChoise(){
+    srand (time(0));
+    const vector<string>units={"km","m","cm","kg","g","ton","hour","min","sec","USD","ILS"};
+    const vector<string>operats={"+","-","==","!=","<=",">=","<",">","+=","-=","*=","++","--","<<","*"}
     string operat;
-    cout << "file found! " << endl;
-    while(true){
-    cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
-    cin >> a;
-    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
-    cin >> b;
-    cout << "choose an operator: (+,-,==,!=,<=,>=,<,>,+=,-=,*=,++,--,<<,exit)" << endl;
-    cin >> operat;
-    if(operat == "+"){
+    for (int i=0;i<20;i++){
+     int b=rand() % 15;
+     operat=operats.at(b);
+     double c=rand()/1000.0;
+     double d=rand()/1000.0;
+     if(b<14){
+       int a1= rand() % 11;
+       int a2= rand() % 11;
+       NumberWithUnits b1{c,units.at(a1)};
+       NumberWithUnits b2{d,units.at(a2)};  
+       if(operat == "+"){
         cout << a << operat << b << '=' << a+b << endl;
     }
     else if(operat == "-"){
@@ -84,6 +78,140 @@ int main() {
         cout << a << operat << '=' << a-- << ' ' << b << '=' << b-- << endl;
     }
     else if(operat == "<<"){
+        cout << "first: " << a << " second: " << b << endl;
+    }
+     }
+      else{
+           int a1= rand() % 11;
+       NumberWithUnits b1{c,units.at(a1)};
+         cout << a << operat << d<<"=" << a*d<< endl;
+      }
+     
+    }
+    
+}
+int main() {
+    string file;
+    get_file(file);
+    
+    ifstream units{file};
+    while(units.fail()){
+        cout << "reading from " << file << " failed." << endl;
+        get_file(file);
+        units.open(file);
+    }
+    NumberWithUnits::read_units(units);
+    NumberWithUnits a;
+    NumberWithUnits b;
+    string operat;
+    cout << "file found! " << endl;
+    while(true){
+    cout << "choose an operator: (+,-,==,!=,<=,>=,<,>,+=,-=,*=,++,--,<<,*,exit)" << endl;
+    cin >> operat;
+    if(operat == "+"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << a+b << endl;
+    }
+    else if(operat == "-"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << a-b << endl;
+    }
+        else if(operat == "*"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "Enter double ";
+     double d;
+    cin >> d;
+        cout << a << operat << d << '=' << a*d << endl;
+    }
+    else if(operat == "=="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a==b) << endl;
+    }
+    else if(operat == "!="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a!=b) << endl;
+    }
+    else if(operat == "<="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a<=b) << endl;
+    }
+    else if(operat == ">="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a>=b) << endl;
+    }
+    else if(operat == "<"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a<b) << endl;
+    }
+    else if(operat == ">"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a>b) << endl;
+    }
+    else if(operat == "+="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a+=b) << endl;
+    }
+    else if(operat == "-="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a-=b) << endl;
+    }
+    else if(operat == "*="){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << b << '=' << (a*=b) << endl;
+    }
+    else if(operat == "++"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << '=' << a++ << ' ' << b << '=' << b++ << endl;
+    }
+    else if(operat == "--"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
+        cout << a << operat << '=' << a-- << ' ' << b << '=' << b-- << endl;
+    }
+    else if(operat == "<<"){
+            cout << "insert unit (format example: 1.1 km or 1.1[km]): ";
+    cin >> a;
+    cout << "insert another unit (format example: 100 cm or 100[cm]): ";
+    cin >> b;
         cout << "first: " << a << " second: " << b << endl;
     }
     else if(operat=="exit"){
